aae767219d8f46f297ee96fd618eb8f2f1327450
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2016-02-12 01:17:57 -0600

    Split navbar into separate file.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -79,45 +79,7 @@
         <!--
         Fixed Navigation
         ==================================== -->
-        <header id="navigation" class="navbar-fixed-top">
-            <div class="container">
-
-                <div class="navbar-header">
-                    <!-- responsive nav button -->
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <!-- /responsive nav button -->
-
-                    <!-- logo -->
-                    <h1 class="navbar-brand">
-                        <a href="#body">
-                         {{--    <img src="img/logo.png" alt="Kasper Logo"> --}}
-                        </a>
-                    </h1>
-                    <!-- /logo -->
-
-                    </div>
-
-                    <!-- main nav -->
-                    <nav class="collapse navigation navbar-collapse navbar-right" role="navigation">
-                        <ul id="nav" class="nav navbar-nav">
-                            <li class="current"><a href="#home">Home</a></li>
-                            {{-- <li><a href="#service">Service</a></li> --}}
-                            <li><a href="#portfolio">Portfolio</a></li>
-                            <li><a href="#about">About</a></li>
-                           {{--  <li><a href="#pricing">Pricing</a></li> --}}
-                            <li><a href="#contact">Contact</a></li>
-                        </ul>
-                    </nav>
-                    <!-- /main nav -->
-                </div>
-
-            
-        </header>
+       @include('partials.navbar')
         <!--
         End Fixed Navigation
         ==================================== -->
@@ -151,11 +113,11 @@
                             </ul>
                         </div>
  --}}
-                        {{-- <div class="copyright">
+                        <div class="copyright">
                            
                             <p>Theme by <a href="http://graphberry.com">GraphBerry.com</a> Developed by <a target="_blank" href="http://www.themefisher.com">Themefisher</a></p>
                         </div>
- --}}
+
                     </div>
                 </div>
             </div>
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,20 +5,25 @@
 
 @section('content')
 
-	{{ Form::open(array('action' => 'HomeController@postLogin')) }}
-<div class="form-group">
-	{{ Form::label('email', 'Email') }}
-	{{ Form::email('email', Input::old('email'), ['class' =>'form-control']) }}
-</div>
-<div "form-group">
-	{{ Form::label('password', 'Password') }}
-	{{ Form::password('password', ['class' => 'form-control']) }}
-</div>
-<div "form-group">
-	{{ Form::submit('Log In', array('class' => 'btn btn-primary'))  }}
-</div>
+<div class="row">
+	<div id="background-for-fund-form" class="col-xm-12 center">
+			{{ Form::open(array('action' => 'HomeController@postLogin')) }}
+		<div class="form-group">
+			{{ Form::label('email', 'Email') }}
+			{{ Form::email('email', Input::old('email'), ['class' =>'form-control']) }}
+		</div>
+		<div "form-group">
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password', ['class' => 'form-control']) }}
+		</div>
+		<div "form-group">
+			{{ Form::submit('Log In', array('class' => 'btn btn-primary'))  }}
+		</div>
 	  
-{{ Form::close() }}
+		{{ Form::close() }}
+	</div>
+</div>
+
 
 @stop
 @section('bottom-script')
